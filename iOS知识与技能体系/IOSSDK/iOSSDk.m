0b9f2de9374ab56a43768a00c3f099d475162229
Master <unclewl@126.com>
2016-05-10 14:30:04 +0800

    iOS知识与技能体系初步归类

---
+++ iOS知识与技能体系/IOSSDK/iOSSDk.m
@@ -0,0 +1,97 @@
+//
+//  iOSSDk.m
+//  iOS知识与技能体系
+//
+//  Created by Master on 16/5/10.
+//
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+
+#pragma mark 编程语言(面向对象)
+ -Objective-C
+    --Object-C运行时特性
+        ---动态性
+    --protocol
+    --category
+    --extension
+    --引用计数
+    --block
+ -C语言
+    --extern 'C'
+ -C++
+    --重载
+    --覆盖
+    --模板类
+    --STL
+        ---vector
+        ---list
+        ---map
+        ---set
+        ---deque
+        ---string
+
+
+
+#pragma mark 分享
+ -EvErNote
+ -论坛
+    --GitHub
+    --CocoaChina
+    --OSchina
+
+
+
+#pragma mark 系统控件
+ -UIKit
+    --自定义控件
+    --换肤
+    --多语言
+    --touches
+        ---手势
+    --info.plist
+        ---Scheme
+
+
+
+#pragma mark 绘图
+ -CoreGraphics
+ -QuatzCore
+    --CALayer
+ -OpenGl ES
+
+
+#pragma mark动画
+ -CoreAn
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
